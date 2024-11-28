7035d76fa74ea40c6ce5957baca69b605c2d22c2
muunji <twoji3019@naver.com>
2024-11-28 17:39:17 +0900

    내용삭제

--- public/index.js
+++
@@ -1,7 +0,0 @@
-// import Handlebars from 'handlebars';
-
-var source = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
-
-var context = { title: "My New Post", body: "This is my first post!" };
-var html = template(context);
